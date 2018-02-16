4ebb6a46f0a653dd6946d423ace0edc53fc67d08
rfsales <fischerr@gmail.com>
2018-02-16 00:56:36 -0200

    Include pt-BR lang corrections
    Include pt-BR lang corrections

--- app/application/language/all.php
+++ app/application/language/all.php
@@ -2,27 +2,55 @@
 $Email["en"] = 'Email';
 $Email["es"] = 'Correo';
 $Email["fr"] = 'Courriel';
+$Email["pt-BR"] = 'Email';
+$Email["it"] = 'Email';
 
 $Language["en"] = 'English';
 $Language["es"] = 'Español';
 $Language["fr"] = 'Français';
+$Language["pt-BR"] = 'Portuguese (Brazil)';
+$Language["it"] = 'Italiano';
 
 $Login["en"] = 'Login';
 $Login["es"] = 'Empezar';
 $Login["fr"] = 'Entrer';
+$Login["pt-BR"] = 'Entrar';
+$Login["it"] = 'Accedi';
 
 $Password["en"] = 'Password';
 $Password["es"] = 'Contraseña';
 $Password["fr"] = 'Mot de passe';
+$Password["pt-BR"] = 'Senha';
+$Password["it"] = 'Password';
 
 $Remember["en"] = 'Remember me';
 $Remember["es"] = 'Me reconocer';
 $Remember["fr"] = 'Se rappeler de moi';
+$Remember["pt-BR"] = 'Lembrar-me';
+$Remember["it"] = 'Ricordami';
 
 $Title["en"] = 'Login to your account';
 $Title["es"] = 'Acceso a su cuenta';
 $Title["fr"] = 'Accéder à votre compte';
+$Title["pt-BR"] = 'Acessar a sua conta';
+$Title["it"] = 'Accedi al tuo account';
 
+$Welcome["de"] = 'Willkommen';
 $Welcome["en"] = 'Welcome to';
 $Welcome["es"] = 'Bienvenido a';
 $Welcome["fr"] = 'Bienvenue chez';
+$Welcome["pt-BR"] = 'Bem Vindo(a)';
+$Welcome["it"] = 'Benvenuto a';
+$Welcome["ru"] = 'Здравствуйте';
+$Welcome["zh-CN"] = '欢迎';
+$Welcome["zh-TW"] = '歡迎';
+
+$WrongPwd["de"] = 'Der Benutzername und das Passwort sind nicht korrekt.';
+$WrongPwd["en"] = 'Whoops, your username or password did not match.';
+$WrongPwd["es"] = 'Usuario o clave no son correctos';
+$WrongPwd["fr"] = 'Oups, votre nom d\'utilisateur et votre mot de passe ne correspondent pas.';
+$WrongPwd["pt-BR"] = 'Oops, seu usuário ou senha estão incorretos.';
+$WrongPwd["it"] = 'Ooops, il tuo nome utente o la tua password non corrispondono.';
+$WrongPwd["ru"] = 'Упсс, имя пользователя и пароль не совпадают.';
+$WrongPwd["zh-CN"] = '哎呀, 您的账号或密码不符';
+$WrongPwd["zh-TW"] = '哎呦, 您的帳號或密碼不符';
